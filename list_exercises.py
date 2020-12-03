a70d2c6f2a97a6247698720930b52d58ea998b1b
hoangtuanvp2011 <t00aienl@gmail.com>
2020-12-03 16:44:23 +0700

    mission is completed

--- list_exercises.py
+++ list_exercises.py
@@ -1,4 +1,5 @@
 def main():
+    # part 1:
     numbers = []
     for i in range(1,6):
         Num = int(input('input number {} :'.format(i)))
@@ -8,5 +9,15 @@ def main():
     print('the smallest number is {}'.format(min(numbers)))
     print('the largest number is {}'.format(max(numbers)))
     print('the average of the numbers is {}'.format(sum(numbers)/len(numbers)))
+
+    # part 2:
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']
+    name = input('enter your name ')
+    if name in usernames:
+        print('Access granted')
+    else:
+        print('Access denied')
 if __name__ == '__main__':
     main()
