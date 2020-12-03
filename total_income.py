f28a9650d458e0386d2380ab6462ba007da1dce2
hoangtuanvp2011 <t00aienl@gmail.com>
2020-12-03 14:45:42 +0700

    complete mission

--- total_income.py
+++ total_income.py
@@ -2,23 +2,25 @@
 CP1404/CP5632 Practical
 Starter code for cumulative total income program
 """
-
+def Calculator(NumberofMonths,incomes):
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month in range(1, NumberofMonths + 1):
+        income = incomes[month - 1]
+        total += income
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
 def main():
-    """Display income report for incomes over a given number of months."""
+    """Display income report for incomes over a given number of Months."""
     incomes = []
-    months = int(input("How many months? "))
+    NumberofMonths = int(input("How many Months? "))
 
-    for month in range(1, months + 1):
+    for month in range(1, NumberofMonths + 1):
         #TODO: the string format() method instead of string concatenation (+).
         income = float(input("Enter income for month {} :".format(month)))
         incomes.append(income)
 
-    print("\nIncome Report\n-------------")
-    total = 0
-    for month in range(1, months + 1):
-        income = incomes[month - 1]
-        total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+    Calculator(NumberofMonths,incomes)
+
 
 if __name__ == '__main__':
     main()
