ef9b6e2169aac62aa162a0571ad785a568c31de9
hoangtuanvp2011 <t00aienl@gmail.com>
2020-12-05 09:53:59 +0700

    reformat code

--- quick_picks.py
+++ quick_picks.py
@@ -1,41 +1,43 @@
 import random
 
+
 def Check(array):
     flag = True
     while flag:
         flag = False
 
-        for i in range(len(array)-1):
+        for i in range(len(array) - 1):
             try:
-                num = array[i+1]
+                num = array[i + 1]
             except IndexError:
                 break
 
             if array[i] == num:
-                array.pop(i+1)
+                array.pop(i + 1)
                 flag = True
 
     return len(array)
 
+
 def main():
     Number = int(input('How many "quick picks" you want: '))
-    Raw =[]
+    Raw = []
     CONSTANTS = []
     for i in range(Number):
 
         for j in range(6):
-            Raw.append(random.randint(1,45))
+            Raw.append(random.randint(1, 45))
             Raw.sort()
         while Check(Raw) < 6:
             Raw.append(random.randint(1, 45))
             Raw.sort()
 
         CONSTANTS.append(Raw)
-        Raw=[]
+        Raw = []
 
     for lines in CONSTANTS:
         for col in range(6):
-            print('{}'.format(lines[col]), end= ' ')
+            print('{}'.format(lines[col]), end=' ')
         print()
 
 
